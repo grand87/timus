1b5786d0d1122c918dc4d793a2cd01dab7a9e723
Volodymyr Sharayenko <grand87@yandex.ru>
2013-05-30 08:39:11 +0300

    Fixed compile errors for linux

--- Problems/1061_Buffer_Manager/src/main.cpp
+++ Problems/1061_Buffer_Manager/src/main.cpp
@@ -6,7 +6,7 @@
 
 #include <stdio.h>
 #include <vector>
-
+#include <string.h>
 //#define ONLINE_JUDGE
 
 struct BlocksSequence
--- Problems/1723_Sandros_Book/src/main.cpp
+++ Problems/1723_Sandros_Book/src/main.cpp
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <vector>
 #include <map>
+#include <string>
 
 using namespace std;
 

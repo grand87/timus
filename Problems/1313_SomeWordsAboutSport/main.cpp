3a9075dfa15a054cae3bc47059201c5424140250
Volodymyr Sharayenko <v.sharayenko@samsung.com>
2016-02-02 20:54:50 +0200

    updated 1313_SomeWordsAboutSport

--- Problems/1313_SomeWordsAboutSport/main.cpp
+++ Problems/1313_SomeWordsAboutSport/main.cpp
@@ -1,36 +1,37 @@
 #include <iostream>
+#include <math.h>
 
 int main()
 {
 #ifndef ONLINE_JUDGE
-	freopen("input.txt", "rt", stdin);
-	freopen("output.txt", "wt", stdout);
+    freopen("input.txt", "rt", stdin);
+    freopen("output.txt", "wt", stdout);
 #endif
-	const unsigned int MaxFrameBufferSize = 100;
-	const float PI = 3.1415926;
-	const float ang45 = PI / 4;
-	const float ang90 = PI / 2;
-	const float ang270 = PI * 1.5;
-	unsigned int frameBuffer[MaxFrameBufferSize][MaxFrameBufferSize];
+    const unsigned int MaxFrameBufferSize = 100;
+    const float PI = 3.1415926;
+    const float ang45 = PI / 4;
+    const float ang90 = PI / 2;
+    const float ang270 = PI * 1.5;
+    unsigned int frameBuffer[MaxFrameBufferSize][MaxFrameBufferSize];
 
-	unsigned int screenSize;
-	std::cin >> screenSize;
+    unsigned int screenSize;
+    std::cin >> screenSize;
 
-	for (int x = 0; x < screenSize; ++x)
-		for (int y = 0; y < screenSize; ++y)
-		{
-			std::cin >> frameBuffer[x][y];
-		}
+    for (unsigned int x = 0; x < screenSize; ++x)
+        for (unsigned int y = 0; y < screenSize; ++y)
+        {
+            std::cin >> frameBuffer[x][y];
+        }
 
-	for (int x = 0; x < screenSize; ++x)
-		for (int y = 0; y < screenSize; ++y)
-		{
-			float targetX = x * cos(ang45) + y * sin(ang45);
-			float targetY = x * sin(ang45) + y * cos(ang45);
+    for (unsigned int x = 0; x < screenSize; ++x)
+        for (unsigned int y = 0; y < screenSize; ++y)
+        {
+            float targetX = x * cos(ang45) - y * sin(ang45);
+            float targetY = x * sin(ang45) + y * cos(ang45);
 
-			unsigned int resX = (unsigned int)targetX;
-			unsigned int resY = (unsigned int)targetY;
+            unsigned int resX = targetX < 0 ? 0 : round(targetX);
+            unsigned int resY = targetY < 0 ? 0 : round(targetY);
 
-			std::cout << frameBuffer[resX][resY] << " ";
-		}
+            std::cout << frameBuffer[resX][resY] << " ";
+        }
 }

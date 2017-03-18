95da4fca6d7550ac9849e6909d261508a6f64f2d
Volodymyr Sharayenko <grandamx@gmail.com>
2017-03-19 00:58:53 +0200

    added 1037

---
+++ Problems/1037_Memory_Management/src/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * @author v.sharayenko (grand87@yandex.ru)
+ */
+
+#pragma comment(linker, "/STACK:16777216")
+
+#include <stdio.h>
+
+using namespace std;
+
+//#define ONLINE_JUDGE
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "rt", stdin);
+    freopen("output.txt", "wt", stdout);
+#endif
+    
+
+    return 0;
+}

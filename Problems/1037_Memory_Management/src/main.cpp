0ef63d96f77a68be5943ff618412adc5f7cd6928
Volodymyr Sharayenko <grand87@yandex.ru>
2017-03-25 21:18:30 +0200

    updated 1037

--- Problems/1037_Memory_Management/src/main.cpp
+++ Problems/1037_Memory_Management/src/main.cpp
@@ -4,11 +4,42 @@
 
 #pragma comment(linker, "/STACK:16777216")
 
-#include <stdio.h>
+#include <string.h>
+#include <iostream>
 
 using namespace std;
 
 //#define ONLINE_JUDGE
+
+struct block
+{
+	bool is_allocated;
+	int timer;
+};
+
+class manager
+{
+	static const int max_block = 80000;
+	static const int max_life_time = 10 * 60;
+	block memory[max_block];
+
+	int free_block_counter;
+
+public:
+
+	manager()
+	{
+		free_block_counter = 0;
+	}
+
+	int allocate(int time)
+	{
+		memory[free_block_counter].is_allocated = true;
+		memory[free_block_counter].timer = time;
+
+		return free_block_counter++ + 1;
+	}
+};
 
 int main()
 {
@@ -17,6 +48,14 @@ int main()
     freopen("output.txt", "wt", stdout);
 #endif
     
+	std::string line;
+	while (std::getline(std::cin, line))
+	{
+
+	}
+
+
+
 
     return 0;
 }

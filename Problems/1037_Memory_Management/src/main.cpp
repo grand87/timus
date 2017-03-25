ad5d55ab56c98c78b87177ea8ac6814d2bcd9925
Volodymyr Sharayenko <grandamx@gmail.com>
2017-03-25 22:19:14 +0200

    raiting 5320...5204
    solved 1037

--- Problems/1037_Memory_Management/src/main.cpp
+++ Problems/1037_Memory_Management/src/main.cpp
@@ -4,8 +4,9 @@
 
 #pragma comment(linker, "/STACK:16777216")
 
-#include <string.h>
+#include <string>
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
@@ -13,31 +14,67 @@ using namespace std;
 
 struct block
 {
+	block()
+	{
+		is_allocated = false;
+		end_time = 0;
+	}
+
 	bool is_allocated;
-	int timer;
+	int end_time;
 };
 
 class manager
 {
-	static const int max_block = 80000;
+	static const int max_block = 30000;
 	static const int max_life_time = 10 * 60;
 	block memory[max_block];
-
-	int free_block_counter;
-
+	
 public:
 
 	manager()
 	{
-		free_block_counter = 0;
+	
 	}
 
 	int allocate(int time)
 	{
-		memory[free_block_counter].is_allocated = true;
-		memory[free_block_counter].timer = time;
+		// need to evaluate all requested blocks & free block where was no access for a period of time
+		// and this block should be return
+		block *block = 0;
+		int block_id = 0;
+		for (block_id = 0; block_id < max_block; ++block_id)
+		{
+			block = &memory[block_id];
+			if (!block->is_allocated)
+			{
+				break;
+			}
+			else
+			{
+				if (block->end_time <= time)
+				{
+					break;
+				}
+			}
+		}
 
-		return free_block_counter++ + 1;
+		//get free block
+		block->is_allocated = true;
+		block->end_time = time + max_life_time;
+
+		return block_id + 1;
+	}
+
+	bool is_availible(int time, int block_id)
+	{
+		block_id = block_id - 1;
+		if (memory[block_id].is_allocated && memory[block_id].end_time > time)
+		{
+			memory[block_id].end_time = time + max_life_time;
+			return true;
+		}
+		return false;
 	}
 };
 
@@ -48,14 +85,28 @@ int main()
     freopen("output.txt", "wt", stdout);
 #endif
     
-	std::string line;
-	while (std::getline(std::cin, line))
-	{
+	manager manager;
 
+	int time;
+	char operation[5];
+	int block_id;
+
+	while (scanf("%d", &time) != EOF)
+	{
+		scanf("%s", operation);
+		if (operation[0] == '+')
+		{
+			block_id = manager.allocate(time);
+			printf("%d\n", block_id);
+		}
+		else
+		{
+			block_id;
+			scanf("%d", &block_id);
+			bool result = manager.is_availible(time, block_id);
+			printf("%s\n", result ? "+" : "-");
+		}
 	}
 
-
-
-
-    return 0;
+	return 0;
 }

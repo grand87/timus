a2411ad337bd2e43bc47582804fd783c125c2343
Volodymyr Sharayenko <grand87@yandex.ru>
2017-11-23 00:18:16 +0200

    Solved problem 1005: rating 5496

--- Problems/1005_StonePile/main.cpp
+++ Problems/1005_StonePile/main.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
 
-long getWeight(const long *s, const long *w, long size)
+long getWeight(long c, const long *w, long size)
 {
 	long wsum = 0;
 	for (long i = 0; i < size; ++i)
-		wsum += s[i] * w[i];
+		wsum += ((c >> i) & 1) * w[i];
 	return wsum;
 }
 
-void dump(long c, long *s, long size, long *w)
+void dump(long c, const long *w, long size)
 {
 	std::cout << c << " : ";
 	for (long i = 0; i < size; ++i)
-		std::cout << s[i] << " ";
-	std::cout << " - " << getWeight(s, w, size) << std::endl;
+	{
+		std::cout << ((c >> i) & 1);
+	}
+	std::cout << " - " << getWeight(c, w, size) << std::endl;
 }
 
 long main()
 {
 #ifndef ONLINE_JUDGE
-	long c = 0;
 	freopen("input.txt", "rt", stdin);
 	//freopen("output.txt", "wt", stdout);
 #endif
-	const long MAX_N = 20;
+	const long MAX_N = 21;
 	long w[MAX_N] = { 0 };
-	long s[MAX_N] = { 0 };
 
 	long n;
 	std::cin >> n;
@@ -39,31 +39,18 @@ long main()
 	}
 
 	long minDiff = total;
-	while (s[n] == 0)
+	const long totalCombination = 1 << n;
+	for (long i = 1; i < totalCombination; ++i)
 	{
+		const long currentWeight1 = getWeight(i, w, n);
+		const long currentWeight2 = total - currentWeight1;
+		const long diff = abs(currentWeight2 - currentWeight1);
 #ifndef ONLINE_JUDGE
-		dump(c++, s, n, w);
+		dump(i, w, n);
+		std::cout << " - diff = " << diff << std::endl;
 #endif
-		for (long i = 0; i < n + 1;++i)
-		{
-			if (s[i] == 0)
-			{
-				for (long j = 0; j < i; ++j)
-				{
-					s[j] = 0;
-				}
-				s[i] = 1;
-				break;
-			}
-		}
-		long currentWeight = getWeight(w, s, n);
-		if (currentWeight != 0 && currentWeight != total)
-		{
-			long diff = total - currentWeight;
-			if (abs(currentWeight - diff) < minDiff)
-				minDiff = abs(currentWeight - diff);
-		}
-
+		if (diff < minDiff)
+				minDiff = diff;
 	}
 	std::cout << minDiff;
 }

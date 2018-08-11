9a8b77be687325bf692e989b5eb6d7c87bff9f72
Volodymyr Sharayenko <v.sharayenko@samsung.com>
2018-08-10 17:41:47 -0700

    Added solution for attribute-parser and added new project abstract-classes-polymorphism

--- Problems/HackerRank/attribute-parser/main.cpp
+++ Problems/HackerRank/attribute-parser/main.cpp
@@ -16,15 +16,33 @@ public:
 
 	void init(const string &value) {
 		stringstream ss(value);
-		char specSymbol;
+		string data;
+		char specSymbol = 0;
 		ss >> specSymbol;
 		ss >> name;
-		ss >> specSymbol;
-		if (specSymbol != '>') { //parsing attributes
-			string aName;
-			string aValue;
-			while (!ss.eof()) {
-				ss >> aName;
+		if (name.back() == '>')
+			name.pop_back();
+		else {
+			ss >> data;
+			if (data.compare(">") != 0) {
+				while (!ss.eof()) {
+					string aName = data;
+					string aValue;
+
+
+					ss >> specSymbol;
+					ss >> specSymbol;
+					ss >> aValue;
+					if (aValue[aValue.length() - 1] == '>') {
+						aValue.pop_back();
+					}
+					aValue.pop_back();
+
+					attrNames.push_back(aName);
+					attrValues.push_back(aValue);
+
+					ss >> data;
+				}
 			}
 		}
 	}
@@ -37,8 +55,38 @@ public:
 		childNames.push_back(tag->getName());
 	}
 
+	const Tag* getTag(const string& name) const {
+		int index = 0;
+		for (const auto& tagName : childNames) {
+			if (tagName.compare(name) == 0)
+				break;
+			++index;
+		}
+		if (index < childTags.size())
+			return childTags[index];
+		return nullptr;
+	}
+
+	bool getValue(const string& attrName, string& value) const {
+		int index = 0;
+		for (const auto& name : attrNames) {
+			if (name.compare(attrName) == 0)
+				break;
+			++index;
+		}
+		if (index < attrValues.size()) {
+			value = attrValues[index];
+			return true;
+		}
+		return false;
+	}
+
 	const string& getName() const {
 		return name;
+	}
+
+	void setName(const string& aName) {
+		name = aName;
 	}
 
 private:
@@ -52,10 +100,16 @@ private:
 };
 
 bool isStartTag(const string& value) {
-	return value[1] != '/';
+	return value[1] != '/' && value[0] != '/';
 }
 
 int main() {
+
+#ifdef LOCAL_TESTS
+	freopen("input.txt", "rt", stdin);
+	freopen("output.txt", "wt", stdout);
+#endif
+
 	int lines = 0;
 	int queries = 0;
 
@@ -67,21 +121,66 @@ int main() {
 
 	cin >> lines >> queries;
 	int currentLevel = 0;
+
+	Tag* newTag = &allTags[uniqID++];
+	newTag->setName("root");
+	currentTag[currentLevel] = newTag;
+
 	for (int i = 0; i < lines; ++i) {
 		string value;
-		cin >> value;
+		while (value.size() == 0 && !cin.eof()) {
+			getline(cin, value);
+		}
 		if (isStartTag(value)) {
-			Tag* newTag = &allTags[uniqID++];
+			newTag = &allTags[uniqID++];
 			newTag->init(value);
-			if (currentLevel != 0) {
-				currentTag[currentLevel]->addTag(newTag);
-			}
-			currentTag[currentLevel++] = newTag;
+			currentTag[currentLevel]->addTag(newTag);
+			currentTag[++currentLevel] = newTag;
 		} else {
 			--currentLevel;
 		}
 	}
 
+	for (int i = 0; i < queries; ++i) {
+		string query;
+		while (query.size() == 0 && !cin.eof()) {
+			getline(cin, query);
+		}
+
+		std::vector<std::string> tagNames;
+		string attrName;
+		{
+			std::string tag;
+			{
+				std::stringstream ss(query);
+				while (getline(ss, tag, '.')) {
+					tagNames.push_back(tag);
+				}
+			}
+			{
+				std::stringstream ss(tagNames.back());
+				getline(ss, tag, '~');
+				ss >> attrName;
+				tagNames.back() = tag;
+			}
+		}
+
+		const Tag* tagNode = currentTag[0];
+		for (const auto& name : tagNames) {
+			tagNode = tagNode->getTag(name);
+			if (tagNode == nullptr) {
+				break;
+			}
+		}
+
+		string attrValue;
+		if (!tagNode || !tagNode->getValue(attrName, attrValue))
+			cout << "Not Found!";
+		else
+			cout << attrValue;
+		if (i + 1 < queries)
+			cout << endl;
+	}
 
 	return 0;
 }

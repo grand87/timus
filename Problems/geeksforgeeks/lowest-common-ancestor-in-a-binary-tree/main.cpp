60c8ab509c73f2952964dd797baa91c0d42a9852
Volodymyr Sharayenko <grand87@yandex.ru>
2019-03-27 18:21:51 -0700

    added some tasks

--- Problems/geeksforgeeks/lowest-common-ancestor-in-a-binary-tree/main.cpp
+++ Problems/geeksforgeeks/lowest-common-ancestor-in-a-binary-tree/main.cpp
@@ -1,49 +1,81 @@
+#include <unordered_map>
+#include <map>
 #include <iostream>
-#include <stack>
-#include <vector>
 
 using namespace std;
-bool isSubSequence(vector<int> s1, vector<int> s2, int m, int n)
+/* A binary tree node has data, pointer to left child
+    and a pointer to right child */
+struct Node
 {
-
-    if (m == 0) return true;
-    if (n == 0) return false;
-
-
-    if (s1[m - 1] == s2[n - 1])
-        return isSubSequence(s1, s2, m - 1, n - 1);
-
-
-    return isSubSequence(s1, s2, m, n - 1);
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+Node * LCA(Node*, int, int);
+/* Helper function that allocates a new node with the
+    given data and NULL left and right pointers. */
+struct Node* newNode(int data)
+{
+    struct Node* node = (struct Node*)
+        malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return(node);
 }
-vector<int> find3Numbers(vector<int>, int);
-// Driver program to test above function
+/* Driver program to test size function*/
 int main()
 {
+
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "rt", stdin);
 #endif
+
     int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        vector<int> a(n + 1);
-        for (int i = 0; i < n; i++)
-            cin >> a[i];
-
-        vector<int> res = find3Numbers(a, n);
-
-        if (res.size() == 0)
+    struct Node *child;
+    scanf("%d", &t);
+        while (t--)
         {
-            cout << 0 << endl;
+            map<int, Node*> m;
+            int n;
+            scanf("%d",&n);
+                struct Node *root = NULL;
+            if (n == 1)
+            {
+                int a;
+                cin >> a;
+                cout << a << endl;
+            }
+            else {
+                while (n--)
+                {
+                    Node *parent;
+                    char lr;
+                    int n1, n2;
+                    scanf("%d %d %c", &n1, &n2, &lr);
+                    //  cout << n1 << " " << n2 << " " << (char)lr << endl;
+                    if (m.find(n1) == m.end())
+                    {
+                        parent = newNode(n1);
+                        m[n1] = parent;
+                        if (root == NULL)
+                            root = parent;
+                    }
+                    else
+                        parent = m[n1];
+                    child = newNode(n2);
+                    if (lr == 'L')
+                        parent->left = child;
+                    else
+                        parent->right = child;
+                    m[n2] = child;
+                }
+                int a, b;
+                cin >> a >> b;
+                Node * k = LCA(root, a, b);
+                cout << k->data << endl;
+            }
         }
-        else if (res[0] < res[1] && res[1] < res[2])
-            cout << isSubSequence(res, a, res.size(), n) << endl;
-        else
-            cout << 0 << endl;
-    }
     return 0;
 }
 
@@ -51,31 +83,52 @@ int main()
 you need to write your solution in the form of Function(s) only.
 Driver Code to call/invoke your function is mentioned above.*/
 
-/*The function returns a vector containing the
-increasing sub-sequence of length 3 if present
-else returns an empty vector */
-vector<int> find3Numbers(vector<int> A, int N)
+/* A binary tree node
+struct Node
 {
-    vector < vector<int> > numbers;
-    int res = -1;
-    numbers.push_back(vector<int>());
-    numbers[0].push_back(A[0]);
-    for (int j = 1; j < N; j++) {
-        bool insterted = false;
-        for (int i = 0; i < numbers.size(); i++) {
-            if (A[j] > numbers[i][numbers[i].size() - 1]) {
-                numbers[i].push_back(A[j]);
-                insterted = true;
-                if (numbers[i].size() == 3)
-                    return numbers[i];
-            }
-        }
-        if (!insterted) {
-            numbers.push_back(vector<int>());
-            numbers[numbers.size() - 1].push_back(A[j]);
-        }
+int data;
+Node* left, * right;
+}; */
+/*you are required to
+complete this function */
+
+typedef map<int, Node*> NodePath;
+
+bool locate(Node* root, int n1, NodePath& path) {
+    if (root == NULL)
+        return false;
+    if (root->data == n1) {
+        path[root->data] = root;
+        return true;
     }
-        
-    return vector<int>();
+    if (locate(root->left, n1, path)) {
+        path[root->data] = root;
+        return true;
+    }
+    else if (locate(root->right, n1, path)) {
+        path[root->data] = root;
+        return true;
+    }
+    return false;
 }
 
+Node * LCA(Node* root, int n1, int n2)
+{
+    NodePath pathToN1;
+
+    //build path as set of nodes to n1
+    locate(root, n1, pathToN1);
+
+    //build path as set of nodes to n2 & search where intersects with n1's path
+    NodePath pathToN2;
+    locate(root, n2, pathToN2);
+
+    for (NodePath::reverse_iterator it = pathToN2.rbegin(); it != pathToN2.rend(); it++) {
+        //check is there such node in the path to n1
+        NodePath::iterator nodeInN1Path = pathToN1.find(it->first);
+        if (nodeInN1Path != pathToN1.end())
+            return nodeInN1Path->second;
+    }
+
+    return nullptr;
+}
